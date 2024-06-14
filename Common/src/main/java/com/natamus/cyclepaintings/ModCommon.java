dd02ad53b3a714a8862630f27db54fd224e3895f
Rick South <rick@natamus.com>
2024-06-14 05:36:38 +0200

    Cycle Paintings source code for 1.21.0.

---
+++ Common/src/main/java/com/natamus/cyclepaintings/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.cyclepaintings;
+
+import com.natamus.cyclepaintings.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
