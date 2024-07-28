d0b20146e1d663dd6e6799043fd1764546461077
Rick South <rick@natamus.com>
2024-07-28 07:37:43 +0200

    (v3.6) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/cyclepaintings/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/cyclepaintings/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.cyclepaintings;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.cyclepaintings.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.cyclepaintings.neoforge.events.NeoForgePaintingEvent;
 import com.natamus.cyclepaintings.util.Reference;
@@ -15,6 +16,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
