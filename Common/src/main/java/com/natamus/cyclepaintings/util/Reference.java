25da23ceb1c6c0c4677537b107aa57652b2d2b49
Rick South <rick@natamus.com>
2024-10-24 20:06:27 +0200

    Cycle Paintings source code for 1.21.2.

---
+++ Common/src/main/java/com/natamus/cyclepaintings/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.cyclepaintings.util;
+
+public class Reference {
+	public static final String MOD_ID = "cyclepaintings";
+	public static final String NAME = "Cycle Paintings";
+	public static final String VERSION = "3.6";
+	public static final String ACCEPTED_VERSIONS = "[1.21.2]";
+}
