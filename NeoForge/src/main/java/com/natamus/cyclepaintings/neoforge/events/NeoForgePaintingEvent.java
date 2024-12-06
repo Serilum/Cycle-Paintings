7ca1c371f3107af36ca6db10c4ab161d5e19eb78
Rick South <rick@natamus.com>
2024-12-06 13:36:42 +0100

    Cycle Paintings source code for 1.21.4.

---
+++ NeoForge/src/main/java/com/natamus/cyclepaintings/neoforge/events/NeoForgePaintingEvent.java
@@ -0,0 +1,34 @@
+package com.natamus.cyclepaintings.neoforge.events;
+
+import com.natamus.cyclepaintings.data.Constants;
+import com.natamus.cyclepaintings.events.PaintingEvent;
+import com.natamus.cyclepaintings.util.Reference;
+import com.natamus.cyclepaintings.util.Util;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.neoforge.event.server.ServerStartedEvent;
+
+@EventBusSubscriber
+public class NeoForgePaintingEvent {
+	@SubscribeEvent
+	public static void onServerStart(ServerStartedEvent e) {
+		try {
+			Util.setPaintings(e.getServer().registryAccess().lookupOrThrow(Registries.PAINTING_VARIANT));
+		}
+		catch (Exception ex) {
+			Constants.logger.warn("[" + Reference.NAME + "] Something went wrong while loading all paintings.");
+		}
+	}
+
+	@SubscribeEvent
+	public static void onClick(PlayerInteractEvent.EntityInteract e) {
+		Player player = e.getEntity();
+		if (PaintingEvent.onClick(player, e.getLevel(), e.getHand(), e.getTarget(), null).equals(InteractionResult.SUCCESS)) {
+			player.swing(e.getHand());
+		}
+	}
+}
